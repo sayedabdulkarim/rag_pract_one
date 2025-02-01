f78207228b1a9e6fcd8463d09252e08cb762045e
Sayed Abdul <sakarim9124@gmail.com>
2025-02-01 09:32:03 +0530

    refactor: restructure hello world program with main function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello world')
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Simple hello world program."""
+
+def main():
+    """Print greeting message."""
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
